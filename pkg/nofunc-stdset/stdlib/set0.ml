100390cbd1d2f41abeedd38efaaea6765bef638a
Mathieu Barbin <mathieu.barbin@gmail.com>
2025-12-08 11:34:02 +0100

    Apply headache script

--- pkg/nofunc-stdset/stdlib/set0.ml
+++ pkg/nofunc-stdset/stdlib/set0.ml
@@ -1,3 +1,9 @@
+(***********************************************************************************)
+(*  nofunc-keyed: Keyed data structures adapted from OCaml Stdlib but no functors  *)
+(*  SPDX-FileCopyrightText: 2025 Mathieu Barbin <mathieu.barbin@gmail.com>         *)
+(*  SPDX-License-Identifier: LGPL-2.1-or-later WITH OCaml-LGPL-linking-exception   *)
+(***********************************************************************************)
+
 (**************************************************************************)
 (*                                                                        *)
 (*                                 OCaml                                  *)
