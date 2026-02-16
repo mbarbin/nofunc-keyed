b2e44863678abab234a686a53fdda513bb56dbe6
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-02-16 12:27:01 +0100

    Initiate skeleton for nofunc-stdhtbl pkg

---
+++ src/nofunc-stdhtbl/hashtbl0.ml
@@ -0,0 +1,11 @@
+(***********************************************************************************)
+(*  nofunc-keyed: Keyed data structures adapted from OCaml Stdlib but no functors  *)
+(*  SPDX-FileCopyrightText: 2025 Mathieu Barbin <mathieu.barbin@gmail.com>         *)
+(*  SPDX-License-Identifier: LGPL-2.1-or-later WITH OCaml-LGPL-linking-exception   *)
+(***********************************************************************************)
+
+open struct
+  (* Only using the dependency to silence @unused-libs. That part of the project
+     hasn't been written yet. *)
+  open! Nofunc_stdhtbl_stdlib.Hashtbl0
+end
