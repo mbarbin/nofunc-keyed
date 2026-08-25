f6122a51471405a8ce8793aadda8fea94910e84a
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-08-25 14:14:13 +0200

    Use mli comment form allowed by linter

--- src/nofunc-htbl/stdlib/hashtbl0.mli
+++ src/nofunc-htbl/stdlib/hashtbl0.mli
@@ -18,23 +18,23 @@
   polymorphic versions. Keep only the former functorized interface but defunc,
   that is require [equal] and [seeded_hash] everywhere needed. *)
 
-(**************************************************************************)
-(*                                                                        *)
-(*                                 OCaml                                  *)
-(*                                                                        *)
-(*             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
-(*                                                                        *)
-(*   Copyright 1996 Institut National de Recherche en Informatique et     *)
-(*     en Automatique.                                                    *)
-(*                                                                        *)
-(*   All rights reserved.  This file is distributed under the terms of    *)
-(*   the GNU Lesser General Public License version 2.1, with the          *)
-(*   special exception on linking described in the file LICENSE.          *)
-(*                                                                        *)
-(**************************************************************************)
+(*_*************************************************************************)
+(*_                                                                        *)
+(*_                                 OCaml                                  *)
+(*_                                                                        *)
+(*_             Xavier Leroy, projet Cristal, INRIA Rocquencourt           *)
+(*_                                                                        *)
+(*_   Copyright 1996 Institut National de Recherche en Informatique et     *)
+(*_     en Automatique.                                                    *)
+(*_                                                                        *)
+(*_   All rights reserved.  This file is distributed under the terms of    *)
+(*_   the GNU Lesser General Public License version 2.1, with the          *)
+(*_   special exception on linking described in the file LICENSE.          *)
+(*_                                                                        *)
+(*_*************************************************************************)
 
-(* NOTE: If this file is hashtbl.mli, do not edit it directly! Instead, edit
-   templates/hashtbl.template.mli and run tools/sync_stdlib_docs *)
+(*_ NOTE: If this file is hashtbl.mli, do not edit it directly! Instead, edit
+  templates/hashtbl.template.mli and run tools/sync_stdlib_docs *)
 
 (** Hash tables and hash functions.
 
